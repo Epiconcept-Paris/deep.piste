2cc4a7186449dee8264c1b4d84e9fbc2b7915b70
Francisco Orchard <f.orchard@epiconcept.fr>
2021-02-19 09:36:11 +0100

    increasing version to 0.0.13

--- dpiste/__init__.py
+++ dpiste/__init__.py
@@ -1,6 +1,6 @@
 
 __package__ = 'dpiste'
-__version__ = '0.0.12'
+__version__ = '0.0.13'
 __author__ = 'Francisco Orchard f.orchard@epiconcept.fr'
 __all__ = []
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
 
 setup(
     name='deep-piste',
-    version='0.0.12',
+    version='0.0.13',
     description='Evaluating the impact of IA on Breast Cancer Screening Program',
     long_description=readme,
     long_description_content_type='text/markdown',
