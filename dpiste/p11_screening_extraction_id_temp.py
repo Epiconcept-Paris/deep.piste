4f78e49fb770c2493defa791cc36f553266c7ed3
Francisco Orchard <f.orchard@epiconcept.fr>
2021-06-29 13:16:51 +0200

    Adding template code for development

---
+++ dpiste/p11_screening_extraction_id_temp.py
@@ -0,0 +1,33 @@
+
+
+def p11_001_generate_signing_key():
+  # randomly generate new private-publuc RSA key
+  # store generaed keys in data/output/hdh/signing-key-public.rsa
+  # store generaed keys in data/output/hdh/signing-key-private.rsa
+  raise NotImplementedError()
+
+def p11_002_generate_encryption_key():
+  # randomly generate new private-publuc RSA key
+  # store generaed keys in data/output/hdh/crypting-key-public.rsa
+  # store generaed keys in data/output/hdh/crypting-key-private.rsa
+  raise NotImplementedError()
+
+def p11_003_encrypt_hdh_extraction_test():
+  # génération d'un fichier csv figé dans data/output/hdh/screening_extraction_dummy.csv
+  raise NotImplementedError()
+
+def p11_004_sygn_hdh_extraction_test():
+  raise NotImplementedError()
+
+def p11_003_encrypt_hdh_extraction():
+  # génération d'un fichier csv data/output/hdh/screening_extraction.csv à partir du resulat de la fonction dal.screening.depistage_pseudo
+  #   pour dévelopement remplacer le fichier data/transform/screening/depistage_pseudo.parquet par un fichier parquet figé.
+  raise NotImplementedError()
+
+def p11_004_sygn_hdh_extraction():
+  raise NotImplementedError()
+
+
+def upload_hdh_extractio_sftp_test():
+
+def upload_hdh_extractio_sftp():
