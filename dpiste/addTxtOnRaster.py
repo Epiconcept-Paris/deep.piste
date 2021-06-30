28e04aa37612ee1bbaf66b96d699191692259797
Mickey-Miky-Low <william.madie0@gmail.com>
2021-06-30 16:51:05 +0200

    move dicom2narray() & isolate summarizeDcmInfo()

--- dpiste/addTxtOnRaster.py
+++ dpiste/addTxtOnRaster.py
@@ -1,4 +1,5 @@
 import random
+from dicom2png import dicom2narray
 import numpy as np
 from pydicom import *
 from PIL import Image, ImageFont, ImageDraw
@@ -8,9 +9,10 @@ import os
 import pydicom
 from pydicom.pixel_data_handlers.util import apply_color_lut, apply_modality_lut, apply_voi_lut
 import cv2 as cv
+from p08_anonymize import *
 
-pathDCM = '/home/williammadie/images/test10/dicom'
-pathPNG = '/home/williammadie/images/test10/png'
+pathDCM = '/home/williammadie/images/test30/dicom'
+pathPNG = '/home/williammadie/images/test30/png'
 pathFonts = '/home/williammadie/images/fonts'
 
 
@@ -25,17 +27,34 @@ test images before treating them with the OCR module.
 
 def main():
     #Converts the DICOM into a numpy array and manages the color problems linked to MONOCHROME2
-    count = 0
-    for file in os.listdir(pathDCM):
-        print(file)
-        pixels = read_xray(pathDCM + "/" + file, count=count)
-        strObtenue = """
+    count = 1
+    random_text = generateRandomTxt()
+    for file in sorted(os.listdir(pathDCM)):
+        pixels = dicom2narray(pathDCM + "/" + file)
+        #summarizeDcmInfo(pixels, file, count)
+        
+        pixels = addTxt2Raster(random_text, random.randint(30,60), pixels, count)
+
+        img = Image.fromarray(pixels)
+        print(file + "-->" + pathPNG + "/preprocess" + str(count) + ".png")
+        img.save(pathPNG + "/preprocess" + str(count) + ".png")
+        count += 1
+
+        get_text_on_picture_easyocr(pixels)
+
+"""
+Development function which prints useful information of the narray (@param pixels)
+"""
+def summarizeDcmInfo(pixels, file, count):
+    strObtenue = """
 Nbre de dimensions : {0},
 Taille dim1, dim2 : {1},
 Nbre total d'éléments : {2},
 Taille dimension 1 : {3},
 (vMin, vMax) contenu dans une cellule : {4},
 MONOCHROME : {5}
+
+==================================================
         """.format(
             pixels.ndim,
             pixels.shape,
@@ -44,28 +63,9 @@ MONOCHROME : {5}
             getVminVmax(pixels),
             pydicom.read_file(pathDCM + "/" + file).PhotometricInterpretation
         )
+    print("==================================================\n")
+    print("Image n°" + str(count) + " : " + file + "\n" + strObtenue )
 
-        print("Image n°" + str(count) + "\n" + strObtenue)
-        
-        img = Image.fromarray(pixels)
-        print(file + "-->" + pathPNG + "/preprocess" + str(count) + ".png")
-        img.save(pathPNG + "/preprocess" + str(count) + ".png")
-        count += 1
-
-    #rasterWthTxt = addTxt2Raster("premier test", 24, pixels)
-    #rasterWthTxt.save(pathPNG + "/img1.png")
-    """
-    count = 0
-    for file in os.listdir(pathDCM):
-        ds = dcmread(file)
-        pixels = ds.pixel_array
-        
-        dicom2png(file, pathPNG + 'test' + count)
-        count += 1
-    """
-
-    #textToAdd = generateRandomTxt()
-    #addTxt2Png(textToAdd)
 
 
 
@@ -87,7 +87,7 @@ def getVminVmax(TwoDimArray):
 
 
 """
-Generates 10 random texts with sample information.
+Generates 20 random texts with sample information.
 """
 def generateRandomTxt():
     patients = generateRandomPatients()
@@ -95,10 +95,10 @@ def generateRandomTxt():
 
     for patient in patients:
         txt = """
-Patient n°{0},
-{1}, 
-Âge : {2} ans, 
-Adresse : {3}
+Patient n°{0},\n
+{1},\n 
+Âge : {2} ans,\n 
+Adresse : {3}\n
         """.format(
             patient.get('id'),
             patient.get('nom'),
@@ -119,14 +119,14 @@ def generateRandomPatients():
 
     patients = []
 
-    with open('./sample_data/nom_patient.txt') as file:
+    with open('/home/williammadie/images/sample_data/nom_patient.txt') as file:
         noms = file.readlines()
 
-    with open('./sample_data/adresse_patient.txt') as file:
+    with open('/home/williammadie/images/sample_data/adresse_patient.txt') as file:
         adresses = file.readlines()
 
-    #Creates 10 new sample patients
-    for i in range(10):
+    #Creates 20 new sample patients
+    for i in range(20):
         id_patient = random.randint(0, 99999999999)
         age = random.randint(0, 100)
         #Adds the name and removes the '\n' at each line
@@ -147,65 +147,29 @@ def generateRandomPatients():
 """
 Writes text on each picture located in the folder path.
 """
-def addTxt2Raster(textToAdd, size, pixels):
+def addTxt2Raster(textToAdd, size, pixels, count):
     
     #Selects a font and sets font parameters (size...)
-    target_font = os.listdir(pathFonts)[0]
-    img_font = ImageFont.truetype(target_font, size=size)
+    target_font = os.listdir(pathFonts)[random.randint(0,len(os.listdir(pathFonts))-1)]
+    print(target_font)
+    img_font = ImageFont.truetype(target_font, size)
 
     #Create a pillow image from the numpy array
     pixels = pixels/255
-    im = Image.fromarray(np.uint8(cm.bone(pixels)*255))
+    im = Image.fromarray(np.uint8((pixels)*255))
     #NB: The following line alters the initial colors. May be removed.
     #im.convert('RGB') 
     
 
     #Adds the text on the pillow image
     draw = ImageDraw.Draw(im)
-    draw.text((5,5), textToAdd)
+    draw.text((2,5), textToAdd[count-1], fill=255, font=img_font)
     #draw.text((5,5), textToAdd[0], (255,255,255), font=font)
-    im.save(pathPNG + "/img_pillow.png")
+    #im.save(pathPNG + "/img_pillow.png")
     del draw
 
     #Converts the pillow image into a numpy array and returns it
     return np.asarray(im)
-
-
-
-"""
-Converts a DICOM into a NUMPY array. Manages the RGB problems happening during the conversion.
-Returns a numpy array which can be handled by Pillow.
-"""
-def read_xray(path, count, voi_lut = True, fix_monochrome = True):
-    dicom = pydicom.read_file(path)
-    
-    # VOI LUT (if available by DICOM device) is used to transform raw DICOM data to "human-friendly" view
-    if voi_lut:
-        #If the modality is CT (Scanner Image) we have to convert the value of the image first with apply_modality
-        #It uses the values of RescaleSlope and RescaleIntercept to convert the values.
-        if dicom.Modality == "CT":
-            data = apply_modality_lut(dicom.pixel_array, dicom)
-            data = apply_voi_lut(data, dicom)
-        else:
-            data = apply_voi_lut(dicom.pixel_array, dicom)
-    else:
-        data = dicom.pixel_array
-
-    # depending on this value, X-ray may look inverted - fix that:
-    if fix_monochrome and dicom.PhotometricInterpretation == "MONOCHROME1":
-        data = np.amax(data) - data
-
-    #If the DICOM are not in one of these two formats, it can bring new problems.
-    if dicom.PhotometricInterpretation != "MONOCHROME2" and dicom.PhotometricInterpretation != "MONOCHROME1":
-        raise ValueError("Photométrie imprévue : " + dicom.PhotometricInterpretation)
-    
-
-    #np.seterr(divide='ignore', invalid='ignore')    <-- blocks the warning raised by DivisonByZero
-    data = data - np.min(data)
-    data = data / np.max(data)
-    data = (data * 255).astype(np.uint8)
-        
-    return data
 
 
 if __name__ == '__main__':
