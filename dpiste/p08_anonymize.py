9c39fa5d971937753361a62a5a4f68d9a801bddb
Mickey-Miky-Low <william.madie0@gmail.com>
2021-07-07 16:07:16 +0200

    remove debug info

--- dpiste/p08_anonymize.py
+++ dpiste/p08_anonymize.py
@@ -1,12 +1,8 @@
 import os
 import numpy as np
-
 from PIL import Image, ImageDraw, ImageFilter
 from easyocr import Reader
-
 from dpiste import dicom2png
-
-
 
 """
 Anonymize a complete directory of DICOM.
@@ -59,16 +55,7 @@ Easy OCR Test function. Gets an image at the path below and gets the text of the
 """
 def get_text_areas(pixels):
     reader = Reader(['fr'])
-    result = reader.readtext(pixels)
-    
-    #Orders the output (= the different text area found in the picture)
-    count = 0
-    for found in result:
-        #print("Zone n°{} | ".format(count), found[1])
-        #print("Zone n°{} | ".format(count), found[0])
-        count += 1
-
-    return result
+    return reader.readtext(pixels)
 
 
 
@@ -90,13 +77,8 @@ def hide_text(pixels, ocr_data, mode = "black"):
     for found in ocr_data:
         #This condition avoids common false positives
         if found[1] != "" and len(found[1]) > 1:
-            #TODO: remove the following (debug info) 
-            print("Tous les points : ", found[0])
             x1, y1 = int(found[0][0][0]), int(found[0][0][1])
             x2, y2 = int(found[0][2][0]), int(found[0][2][1])
-            print("Point en haut à gauche : ", x1, y1)
-            print("Point en bas à droite : ", x2, y2)
-
             box = (x1,y1,x2,y2)
             
             #Applies a hiding effect
@@ -110,10 +92,8 @@ def hide_text(pixels, ocr_data, mode = "black"):
                 draw = ImageDraw.Draw(im)
 
                 #If the value is a tuple, the color has to be a tuple (RGB image)
-                if type(pixels[0][0]) == tuple:
-                    color = (0,0,0)
-                else:
-                    color = 0
+                color = (0,0,0) if type(pixels[0][0]) == tuple else color = 0
+                    
                 draw.rectangle([x1, y1, x2, y2], fill=color)
                 del draw
 
